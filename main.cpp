d21efdfa0b86c0d7af1642a3bd7ecc4ac911818f
Ronan Fim <ronan.fim@gmail.com>
2020-05-08 00:48:30 -0300

    Drawing a circle to test

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "SFML Test");
 
+    sf::CircleShape cShape(100.f);
+    cShape.setFillColor(sf::Color::Green);
+
     while(window.isOpen())
     {
         sf::Event event;
@@ -15,6 +18,10 @@ int main()
             if(event.type == sf::Event::Closed)
                 window.close();
         }
+
+        window.clear();
+        window.draw(cShape);
+        window.display();
     }
 
     return 0;
